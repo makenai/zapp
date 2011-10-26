e748c39dbef93f5c09e25a4c23c9e7bd8c29b842
Pawel Szymczykowski <makenai@gmail.com>
2011-10-25 22:23:00 -0700

    Dumb proof of concept

---
+++ server.js
@@ -0,0 +1,41 @@
+/*
+
+  Z A P = Zappos Api Proxy Pipelien
+  
+*/
+
+var express = require('express'),
+  http = require('http'),
+  app = express.createServer();
+
+
+API_KEY = 'a24b50f373803eb6b205cf89eee94df8e427ef49'
+    
+// Image proxy thing    
+app.get('/image/:styleId-:type-:recipe', function(req, res){
+
+  var uri = {
+    host: 'api.zappos.com',
+    port: 80,
+    path: '/Image?key=' + API_KEY + '&styleId=' + req.params.styleId + '&type=' + req.params.type + '&recipe=' + req.params.recipe
+  };
+
+  var data = '';
+  http.get(uri, function(get_res) {
+    get_res.on('data', function(chunk) {
+      data += chunk;
+    }).on('error', function(e) {
+      console.log( "Got error: " + e.message );
+    }).on('end', function(chunk) {
+      var images = JSON.parse( data );
+      var image = images[ req.params.styleId ][0].filename;
+      if ( image ) {
+        res.redirect( image );
+      }
+    });
+  });
+
+});
+
+app.listen(8080);
+console.log('Express server started on port %s', app.address().port);
